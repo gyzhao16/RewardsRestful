92d82758170d1c148af8dab7e9dddf247696c8a7
Guangyuan Zhao <guangyua@usc.edu>
2023-09-19 01:05:35 -0700

    Initial Commit

---
+++ src/main/java/com/example/rewardsrestful/RewardsRestfulApplication.java
@@ -0,0 +1,13 @@
+package com.example.rewardsrestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardsRestfulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RewardsRestfulApplication.class, args);
+    }
+
+}
